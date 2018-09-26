8a4dc2c2da5e48eb4a8de9da8b992592b79805e6
김휘겸 <breakstorm00@gmail.com>
2018-09-26 15:28:53 +0900

    Update ToDo리스트 기능 추가, update, delete, read.

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
+// TODO: form-button 태그 사용방법
+// TODO: module에 컨트롤러를 삽입하는 방법.
+
 (function () {
   var app = angular.module('todo', []);
-  app.controller('todoCtrl', function ($scope) {
+
+  app.controller('todoCtrl', function ($window, $scope) {
     $scope.name = 'what the hell';
+    $scope.list = [{
+        'id': 1,
+        'content': 'hello',
+        'done': false
+      },
+      {
+        'id': 2,
+        'content': 'world',
+        'done': false
+      },
+      {
+        'id': 3,
+        'content': 'angularjs',
+        'done': true
+      },
+    ]
+
+    $scope.deleteItem = function (index) {
+      // $scope.list[index].done = !$scope.list[index].done;
+      $scope.list = $scope.list.filter((v, i, a) => {
+        return i !== index;
+      })
+      // $window.console.log($scope.list.filter((v, i, a) => {
+      //   // if (i !== index) return v;
+      //   return i !== index;
+      // }));
+      $window.console.log('delete done');
+    };
+
+    $scope.doneItem = function (index) {
+      $scope.list[index].done = !$scope.list[index].done;
+      $window.console.log('done done');
+    };
+
   });
+
+  app.component('todoList', {
+    template: `List Component, {{$ctrl.list}}`,
+    controller: function () {
+      this.list = 'hello';
+    }
+  });
+
 })();
