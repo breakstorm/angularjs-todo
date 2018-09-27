51bda141df496ecd4b3d64212989a41e617e647d
김휘겸 <breakstorm00@gmail.com>
2018-09-27 23:48:35 +0900

    Update todoStorage function. 기능 모듈화.

--- app.js
+++ app.js
@@ -8,49 +8,21 @@
     $window.console.log(todoStorage.get());
     $scope.name = 'what the hell';
     $scope.list = todoStorage.get();
-    // $scope.list = [{
-    //     'id': 1,
-    //     'content': 'hello',
-    //     'done': false
-    //   },
-    //   {
-    //     'id': 2,
-    //     'content': 'world',
-    //     'done': false
-    //   },
-    //   {
-    //     'id': 3,
-    //     'content': 'angularjs',
-    //     'done': true
-    //   },
-    // ]
 
     $scope.deleteItem = function (index) {
-      // $scope.list[index].done = !$scope.list[index].done;
-      $scope.list = $scope.list.filter((v, i, a) => {
-        return i !== index;
-      })
-      // $window.console.log($scope.list.filter((v, i, a) => {
-      //   // if (i !== index) return v;
-      //   return i !== index;
-      // }));
+      $scope.list = todoStorage.delete(index);
       $window.console.log('delete done');
     };
 
     $scope.doneItem = function (index) {
-      $scope.list[index].done = !$scope.list[index].done;
+      $scope.list = todoStorage.done(index);
       $window.console.log('done done');
     };
 
     $scope.createItem = function () {
       const newValue = $window.document.querySelector('.create-item').value;
-      const newItem = {
-        'id': $scope.list.length + 1,
-        'content': newValue,
-        'done': false
-      }
-      $scope.list.push(newItem);
-      $window.console.log('create done', newItem);
+      $scope.list = todoStorage.create(newValue);
+      $window.console.log('create done');
     }
 
   });
--- storage.js
+++ storage.js
@@ -18,6 +18,28 @@ angular.module('todo').factory('todoStorage', function () {
 
     get: function () {
       return storage.list;
+    },
+
+    create: function (newValue) {
+      const newItem = {
+        'id': storage.list.length + 1,
+        'content': newValue,
+        'done': false
+      };
+      storage.list.push(newItem);
+      // return storage.list;
+    },
+
+    delete: function (index) {
+      storage.list = storage.list.filter((v, i, a) => {
+        return i !== index;
+      });
+      // return storage.list;
+    },
+
+    done: function (index) {
+      storage.list[index].done = !storage.list[index].done;
+      // return storage.list;
     }
   }
 
